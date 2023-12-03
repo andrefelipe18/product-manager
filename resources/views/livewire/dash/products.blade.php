7329f9db96022f6a919b56b628b8795e9631421a
Adryel Dearo <adryelzd77@outlook.com>
2023-12-03 00:22:20 -0300

    Add daisyui package and update views

--- resources/views/livewire/dash/products.blade.php
+++ resources/views/livewire/dash/products.blade.php
@@ -1,25 +1,27 @@
-<div>
-    <div>
-        <label for="add-product">Adicionar produto</label>
-        <input type="text" id="add-product" wire:model="name">
-        <button wire:click="add">Adicionar</button>
+<div class="flex flex-col">
+    <div class="m-5">
+        <label class="label-text" for="add-product">Adicionar produto</label>
+        <input class="input input-sm input-bordered w-full max-w-xs" type="text" id="add-product" wire:model="name">
+        <button class="btn btn-sm btn-neutral" wire:click="add">Adicionar</button>
     </div>
-    <table class="p-6 text-gray-900 dark:text-gray-100">
-        <thead>
-            <tr class="bg-gray-100 dark:bg-gray-600">
-                <th class="px-4 py-2">ID</th>
-                <th class="px-4 py-2">Name</th>
-                <th class="px-4 py-2">Created At</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($products as $product)
-            <tr class="bg-gray-100 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600 hover:bg-gray-200 dark:hover:bg-gray-600">
-                <td class="px-4 py-2">{{ $product->id }}</td>
-                <td class="px-4 py-2">{{ $product->name }}</td>
-                <td class="px-4 py-2">{{ $product->created_at }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="overflow-x-auto">
+        <table class="table table-lg table-pin-rows table-pin-cols">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Created At</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($products as $product)
+                <tr>
+                    <td>{{ $product->id }}</td>
+                    <td>{{ $product->name }}</td>
+                    <td>{{ $product->created_at }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
--- resources/views/livewire/dash/users.blade.php
+++ resources/views/livewire/dash/users.blade.php
@@ -1,22 +1,24 @@
 <div>
-  <table class="p-6 text-gray-900 dark:text-gray-100">
-    <thead>
-        <tr class="bg-gray-100 dark:bg-gray-600">
-            <th class="px-4 py-2">ID</th>
-            <th class="px-4 py-2">Name</th>
-            <th class="px-4 py-2">Email</th>
-            <th class="px-4 py-2">Created At</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($users as $user)
-        <tr class="bg-gray-100 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600 hover:bg-gray-200 dark:hover:bg-gray-600">
-            <td class="px-4 py-2">{{ $user->id }}</td>
-            <td class="px-4 py-2">{{ $user->name }}</td>
-            <td class="px-4 py-2">{{ $user->email }}</td>
-            <td class="px-4 py-2">{{ $user->created_at }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
+  <div class="overflow-x-auto">
+    <table class="table table-md table-pin-rows table-pin-cols">
+      <thead>
+          <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Created At</th>
+          </tr>
+      </thead>
+      <tbody>
+          @foreach($users as $user)
+          <tr>
+              <td>{{ $user->id }}</td>
+              <td>{{ $user->name }}</td>
+              <td>{{ $user->email }}</td>
+              <td>{{ $user->created_at }}</td>
+          </tr>
+          @endforeach
+      </tbody>
+    </table>
+  </div>
 </div>
