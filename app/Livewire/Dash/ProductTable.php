52c330fe81a16141cf3674fa03a6d3bd852bdada
Adryel Dearo <adryelzd77@outlook.com>
2023-12-03 15:34:59 -0300

    Add Laravel Livewire Tables package

---
+++ app/Livewire/Dash/ProductTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire\Dash;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\Product;
+use Illuminate\Support\Facades\Date;
+use Rappasoft\LaravelLivewireTables\Views\Filters\DateFilter;
+use Rappasoft\LaravelLivewireTables\Views\Filters\MultiSelectDropdownFilter;
+
+class ProductTable extends DataTableComponent
+{
+    protected $model = Product::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id')
+            ->setTableRowUrl(function ($row) {
+                return route('dashboard', $row);
+            });
+        
+        $this->setSearchEnabled();
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("ID", "id")
+                ->sortable(),
+            Column::make("Nome", "name")
+                ->sortable(),
+            Column::make("Criado em", "created_at")
+                ->sortable(),
+        ];
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,14 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg relative">
-                <x-dashboard.top-nav/>
+                {{-- <x-dashboard.top-nav/> --}}
 
                 <div class="grid grid-col-2 grid-flow-col gap-3 m-5 mb-14">
                     
-                    <livewire:dash.users/>
+                    {{-- <livewire:dash.users/>
                     
-                    <livewire:dash.products/>
+                    <livewire:dash.products/> --}}
                     
+                    {{-- <livewire:dash.UserTable/> --}}
+
+                    <livewire:dash.ProductTable/>
+
                 </div>
 
                 <x-dashboard.btm-nav/>
