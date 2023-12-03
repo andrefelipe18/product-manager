1c6393d1f7dcaf8218531ae240b7cb844747c775
Adryel Dearo <adryelzd77@outlook.com>
2023-12-02 23:14:46 -0300

    [ADD] Users component to dashboard view

---
+++ resources/views/livewire/dash/users.blade.php
@@ -0,0 +1,22 @@
+<div>
+  <table>
+    <thead>
+        <tr class="bg-gray-100 dark:bg-gray-600">
+            <th class="px-4 py-2">ID</th>
+            <th class="px-4 py-2">Name</th>
+            <th class="px-4 py-2">Email</th>
+            <th class="px-4 py-2">Created At</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($users as $user)
+        <tr class="bg-gray-100 dark:bg-gray-700 border-b border-gray-200 dark:border-gray-600 hover:bg-gray-200 dark:hover:bg-gray-600">
+            <td class="px-4 py-2">{{ $user->id }}</td>
+            <td class="px-4 py-2">{{ $user->name }}</td>
+            <td class="px-4 py-2">{{ $user->email }}</td>
+            <td class="px-4 py-2">{{ $user->created_at }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+</div>
